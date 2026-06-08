24fdd85eb8b35a43571c6190aed4f6602d2b989b
RONALD PINEDA <ronald.rgpineda@gmail.com>
2026-06-08 15:05:34 -0600

    Add SQL queries for employee analysis
    Added initial SQL analysis queries to retrieve employee statistics.

---
+++ analysis_01.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) AS total_employees
+FROM employees;
+
+SELECT AVG(salary) AS average_salary
+FROM employees;
+
+SELECT MAX(salary) AS highest_salary
+FROM employees;
+
+SELECT department, COUNT(*) AS total
+FROM employees
+GROUP BY department;
+
+Add first SQL analysis queries
