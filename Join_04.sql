eac21cf442c767c6eb8730eedaf29c9e9283fd8f
RONALD PINEDA <ronald.rgpineda@gmail.com>
2026-06-22 17:58:51 -0600

    Delete Join_04.sql

--- Join_04.sql
+++
@@ -1,7 +0,0 @@
-SELECT employees.name,
-       departments.department
-FROM employees
-JOIN departments
-ON employees.employee_id = departments.employee_id
-WHERE departments.department = 'Sales'
-OR departments.department = 'Marketing';
