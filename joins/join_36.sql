33bc1581a35fff579f346b8e78c0329ec5b141ad
RONALD PINEDA <ronald.rgpineda@gmail.com>
2026-06-25 07:14:13 -0600

    Add JOIN exercise 36

---
+++ joins/join_36.sql
@@ -0,0 +1,7 @@
+SELECT employees.name,
+       departments.department
+FROM employees
+JOIN departments
+ON employees.employee_id = departments.employee_id
+WHERE departments.department <> 'Sales'
+ORDER BY employees.employee_id ASC;
