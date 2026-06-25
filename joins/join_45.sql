bcfd84cd62491e301b601baf2759efd4f2e11370
RONALD PINEDA <ronald.rgpineda@gmail.com>
2026-06-25 13:08:40 -0600

    Add JOIN exercise 45

---
+++ joins/join_45.sql
@@ -0,0 +1,11 @@
+SELECT employees.name,
+       departments.department,
+       salaries.salary
+FROM employees
+JOIN departments
+ON employees.employee_id = departments.employee_id
+JOIN salaries
+ON employees.employee_id = salaries.employee_id
+WHERE departments.department = 'Marketing'
+OR salaries.salary > 1300
+ORDER BY salaries.salary DESC;
