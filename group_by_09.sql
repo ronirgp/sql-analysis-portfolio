63af87424bd5fc3f2c2bb8163eb9a345222bf5b4
RONALD PINEDA <ronald.rgpineda@gmail.com>
2026-06-26 13:06:19 -0600

    Add GROUP BY exercise 09

---
+++ group_by_09.sql
@@ -0,0 +1,8 @@
+SELECT department,
+       AVG(salary),
+       COUNT(*)
+FROM departments
+JOIN salaries
+ON departments.employee_id = salaries.employee_id
+WHERE salary >= 1300
+GROUP BY department;
