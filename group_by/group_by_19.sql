5802b0509f2b4e8e4fde30f8415f627982bfc45e
Ronald <ronald.rgpineda@gmail.com>
2026-06-27 13:29:16 -0600

    Add GROUP BY exercise 19

---
+++ group_by/group_by_19.sql
@@ -0,0 +1,8 @@
+SELECT departments.department,
+       MIN(salaries.salary) AS min_salary,
+       SUM(salaries.salary) AS total_salary
+FROM departments
+JOIN salaries
+ON departments.employee_id = salaries.employee_id
+GROUP BY departments.department
+HAVING SUM(salaries.salary) >= 1500;
