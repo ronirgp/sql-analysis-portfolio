7b69bcd679edeee00cba5c516aa741ea50bfd46d
Ronald <ronald.rgpineda@gmail.com>
2026-06-30 07:21:35 -0600

    Add GROUP BY exercise 31

---
+++ group_by/group_by_31.sql
@@ -0,0 +1,10 @@
+SELECT departments.department,
+       COUNT(*) AS employees,
+       AVG(salaries.salary) AS avg_salary,
+       MIN(salaries.salary) AS min_salary
+FROM departments
+JOIN salaries
+ON departments.employee_id = salaries.employee_id
+WHERE salaries.salary >= 1200
+GROUP BY departments.department
+HAVING AVG(salaries.salary) >= 1300;
