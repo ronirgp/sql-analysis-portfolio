dbbe5ca038c2385deced88f90f808c17cbf997a9
Ronald <ronald.rgpineda@gmail.com>
2026-07-02 07:30:41 -0600

    Add GROUP BY exercise 48

---
+++ group_by/group_by_48.sql
@@ -0,0 +1,11 @@
+SELECT departments.department,
+       COUNT(*) AS employees,
+       AVG(salaries.salary) AS avg_salary,
+       MIN(salaries.salary) AS min_salary,
+       MAX(salaries.salary) AS max_salary
+FROM departments
+JOIN salaries
+ON departments.employee_id = salaries.employee_id
+GROUP BY departments.department
+HAVING COUNT(*) >= 2
+   OR MIN(salaries.salary) >= 1500;
