eb12d038dffea395203526088e16882ab0ec2f8f
Ronald <ronald.rgpineda@gmail.com>
2026-07-05 08:39:24 -0600

    Add GROUP BY exercise 99

---
+++ group_by/group_by_99.sql
@@ -0,0 +1,14 @@
+SELECT departments.department,
+       COUNT(*) AS employees,
+       SUM(salaries.salary) AS total_salary,
+       AVG(salaries.salary) AS avg_salary,
+       MIN(salaries.salary) AS min_salary,
+       MAX(salaries.salary) AS max_salary
+FROM departments
+JOIN salaries
+ON departments.employee_id = salaries.employee_id
+WHERE salaries.salary >= 1300
+GROUP BY departments.department
+HAVING SUM(salaries.salary) >= 3000
+   OR AVG(salaries.salary) >= 1600
+   OR MIN(salaries.salary) >= 1500;
