398b32c29a75a54f68dc9cdf8bb0ee870dbaca88
Ronald <ronald.rgpineda@gmail.com>
2026-07-12 13:08:47 -0600

    Create employee report using LIKE contains pattern

---
+++ 05_SQL_Writing_Practice/86_sql_writing.sql
@@ -0,0 +1,23 @@
+-- ===========================================
+-- SQL Writing Exercise 86
+-- Business Request:
+-- The HR Manager wants a report showing:
+-- - Employee name
+-- - Department name
+-- - Salary
+-- Show only employees whose names
+-- contain:
+-- ar
+-- Use LIKE.
+-- Sort by:
+-- 1. Employee name (A-Z)
+-- ===========================================\
+-- My Solution
+SELECT employees.name,
+       departments.department_name,
+       employees.salary
+FROM employees
+JOIN departments
+ON employees.department_id = departments.department_id
+WHERE employees.name LIKE '%ar%'
+ORDER BY employees.name ASC;
