13c958afb7e4ae8b8a145cb13c1b69fa8ba0f663
Ronald <ronald.rgpineda@gmail.com>
2026-07-15 07:28:45 -0600

    Solve SQL subquery excluding highest salary

--- 03_Subqueries/17_Subqueries_Basics.sql
+++ 03_Subqueries/17_Subqueries_Basics.sql
@@ -1,25 +1,29 @@
--- ===========================================
--- SQL Subquery Exercise 112
--
+-- ==========-- ===========================================
+-- SQL Subquery Exercise 17
 -- Business Request:
--- The Finance Manager wants
--- employees earning exactly
--- the highest salary OR
--- exactly the lowest salary.
+-- The HR Director wants employees
+-- earning MORE than the average
+-- salary but NOT the highest salary.
 -- Show:
 -- - Employee name
 -- - Salary
--- Sort by salary
--- from highest to lowest.
+-- Sort by:
+-- 1. Salary (highest to lowest)
+-- 2. Employee name (A-Z)
 -- ===========================================
 -- My Solution
 SELECT employees.name,
-employees.salary
+       employees.salary
 FROM employees
-WHERE employees.salary =
-(SELECT MAX(salary)
-FROM employees)
-OR employees.salary =
-(SELECT MIN(salary)
-FROM employees)
-ORDER BY  employees.salary DESC;
+WHERE employees.salary >
+(
+    SELECT AVG(salary)
+    FROM employees
+)
+AND employees.salary <
+(
+    SELECT MAX(salary)
+    FROM employees
+)
+ORDER BY employees.salary DESC,
+         employees.name ASC;
