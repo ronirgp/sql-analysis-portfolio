15238b2efeb76559d71174c0c1a073bc3ef80a3d
Ronald <ronald.rgpineda@gmail.com>
2026-07-16 07:54:11 -0600

    Practice multiple-row subquery using not equal comparison

---
+++ 04_Subqueries_Multiple_Rows/10_Multiple_Row_Subqueries.sql
@@ -0,0 +1,30 @@
+-- ===========================================
+-- SQL Multiple-Row Subquery Exercise 10
+-- Business Request:
+-- Show employees working in:
+-- IT
+-- Marketing
+-- Show:
+-- - Employee name
+-- - Salary
+-- Only include employees
+-- earning NOT equal to
+-- the company average salary.
+-- Use:
+-- - One multiple-row subquery
+-- - One single-value subquery
+-- Sort by:
+-- 1. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT employees.name,
+employees.salary
+FROM employees
+WHERE department_id IN 
+(SELECT  department_id 
+FROM departments 
+WHERE department_name IN ('IT', 'Marketing'))
+AND employees.salary <>
+(SELECT AVG(salary)
+FROM employees)
+ORDER BY employees.name ASC;
