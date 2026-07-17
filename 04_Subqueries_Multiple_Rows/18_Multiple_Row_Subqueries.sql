54fdc0ffd69ca72b1e00359bfafb3e4228d1b00e
Ronald <ronald.rgpineda@gmail.com>
2026-07-17 10:23:55 -0600

    Complete multiple-row subquery exercise with OR salary conditions

---
+++ 04_Subqueries_Multiple_Rows/18_Multiple_Row_Subqueries.sql
@@ -0,0 +1,38 @@
+-- ===========================================
+-- SQL Multiple-Row Subquery Exercise 18
+--
+-- Business Request:
+-- Show employees working in:
+-- Marketing
+-- Sales
+-- Show:
+-- - Employee name
+-- - Salary
+-- Only include employees
+-- earning LESS than
+-- the company maximum salary
+-- AND GREATER than
+-- the company average salary.
+-- Use:
+-- - One multiple-row subquery
+-- - Two single-value subqueries
+-- Sort by:
+-- 1. Salary (highest to lowest)
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT employees.name,
+employees.salary
+FROM employees
+WHERE department_id IN 
+(SELECT department_id
+FROM departments
+WHERE department_name IN ('Marketing,', 'Sales'))
+AND employees.salary <
+(SELECT MAX(salary)
+FROM employees )
+AND employees.salary >
+(SELECT AVG(salary)
+FROM employees)
+ORDER BY employees.salary DESC,employees.name ASC;
+
