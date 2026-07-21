2eb74d24af2646fa3b71a4383d06250ec5772201
Ronald <ronald.rgpineda@gmail.com>
2026-07-21 07:37:09 -0600

    Practice filtering between company minimum and maximum salaries

---
+++ 04_Subqueries_Multiple_Rows/33_Multiple_Row_Subqueries.sql
@@ -0,0 +1,41 @@
+-- ===========================================
+-- SQL Multiple-Row Subquery Exercise 33
+--
+-- Business Request:
+--
+-- Show employees working in:
+--
+-- Sales
+-- IT
+-- Show:
+-- - Employee name
+-- - Salary
+-- Only include employees
+-- earning GREATER than
+-- the company minimum salary
+-- AND LESS than
+-- the company maximum salary.
+-- Use:
+-- - One multiple-row subquery
+-- - Two single-value subqueries
+-- Sort by:
+-- 1. Salary (highest to lowest)
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT employees.name,
+employees.salary
+FROM employees
+WHERE department_id IN 
+(SELECT department_id
+FROM departments
+WHERE department_name IN ('Sales', 'IT'))
+AND 
+(employees.salary >
+(SELECT MIN(salary)
+FROM employees))
+AND 
+(employees.salary <
+(SELECT MAX(salary)
+FROM employees))
+ORDER BY employees.salary DESC,  employees.name ASC;
