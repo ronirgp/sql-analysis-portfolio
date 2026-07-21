a76b8a83c505af8f134ef7b1d59ad5a47e1d4179
Ronald <ronald.rgpineda@gmail.com>
2026-07-21 13:09:30 -0600

    Practice filtering between company minimum and average salaries for IT and Marketing

---
+++ 04_Subqueries_Multiple_Rows/38_Multiple_Row_Subqueries.sql
@@ -0,0 +1,39 @@
+-- ===========================================
+-- SQL Multiple-Row Subquery Exercise 38
+--
+-- Business Request:
+-- Show employees working in:
+-- Marketing
+-- IT
+-- Show:
+-- - Employee name
+-- - Salary
+-- Only include employees
+-- earning GREATER than
+-- the company minimum salary
+-- AND LESS than
+-- the company average salary.
+-- Use:
+-- - One multiple-row subquery
+-- - Two single-value subqueries
+-- Sort by:
+-- 1. Salary (lowest to highest)
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT employees.name,
+employees.salary
+FROM employees
+WHERE department_id IN 
+(SELECT department_id 
+FROM departments
+WHERE department_name IN ('Marketing', 'IT'))
+AND 
+(employees.salary >
+(SELECT MIN(salary)
+FROM employees))
+AND
+(employees.salary <
+(SELECT AVG(salary)
+FROM employees))
+ORDER BY employees.salary ASC, employees.name ASC;
