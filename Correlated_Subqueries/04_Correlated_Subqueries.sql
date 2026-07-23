e9e68937034af401ad4a51475b7e58517cbc4b34
Ronald <ronald.rgpineda@gmail.com>
2026-07-23 07:21:47 -0600

    Practice correlated subquery to find highest paid employee in each department

---
+++ Correlated_Subqueries/04_Correlated_Subqueries.sql
@@ -0,0 +1,26 @@
+-- ===========================================
+-- Correlated Subquery Exercise 4
+-- Business Request:
+-- Show employees who earn the
+-- HIGHEST salary in THEIR OWN department.
+-- Show:
+-- - Employee name
+-- - Department ID
+-- - Salary
+-- Use:
+-- - One correlated subquery
+-- Sort by:
+-- 1. Department ID
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.department_id,
+e1.salary
+FROM employees e1
+WHERE e1.salary =
+(SELECT MAX(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+ORDER BY e1.department_id, e1.name ASC;
