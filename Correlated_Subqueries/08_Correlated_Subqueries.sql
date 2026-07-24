f24bdb6e305ad0d8cfdcc0a65943356ff60d06c4
Ronald <ronald.rgpineda@gmail.com>
2026-07-24 07:13:50 -0600

    Practice correlated subqueries combining department average and maximum salary conditions

---
+++ Correlated_Subqueries/08_Correlated_Subqueries.sql
@@ -0,0 +1,35 @@
+-- ===========================================
+-- Correlated Subquery Exercise 8
+-- Business Request:
+-- Show employees who earn
+-- MORE than the average salary
+-- of their own department
+-- BUT are NOT the highest-paid employee
+-- in that department.
+-- Show:
+-- - Employee name
+-- - Department ID
+-- - Salary
+-- Use:
+-- - One correlated subquery for AVG
+-- - One correlated subquery for MAX
+-- Sort by:
+-- 1. Department ID
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.department_id,
+e1.salary
+FROM employees e1
+WHERE e1.salary >
+(SELECT AVG(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+AND e1.salary <>
+(SELECT MAX(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+ORDER BY e1.department_id, e1.name ASC;
