dca72887ccde181b8bb82265d6ab055ff5c3f20a
Ronald <ronald.rgpineda@gmail.com>
2026-07-24 07:41:04 -0600

    Practice correlated subquery using greater than or equal to department average

---
+++ Correlated_Subqueries/10_Correlated_Subqueries.sql
@@ -0,0 +1,28 @@
+-- ===========================================
+-- Correlated Subquery Exercise 10
+-- Business Request:
+-- Show employees whose salary is
+-- GREATER THAN OR EQUAL TO
+-- the average salary
+-- of their own department.
+-- Show:
+-- - Employee name
+-- - Department ID
+-- - Salary
+-- Use:
+-- - One correlated subquery
+-- Sort by:
+-- 1. Department ID
+-- 2. Salary (highest to lowest)
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.department_id,
+e1.salary
+FROM employees e1
+WHERE e1.salary >=
+(SELECT AVG(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+ORDER BY e1.department_id, e1.salary DESC;
