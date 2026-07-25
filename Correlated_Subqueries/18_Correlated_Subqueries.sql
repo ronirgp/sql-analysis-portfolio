7b1bb8a5946f8b59152b4c6fe2253c51bb745c64
Ronald <ronald.rgpineda@gmail.com>
2026-07-25 07:42:37 -0600

    Practice correlated subquery excluding department average and minimum salary

---
+++ Correlated_Subqueries/18_Correlated_Subqueries.sql
@@ -0,0 +1,39 @@
+-- ===========================================
+-- Correlated Subquery Exercise 18
+--
+-- Business Request:
+-- Show employees whose salary is
+-- NOT EQUAL TO
+-- the average salary
+-- of their own department
+-- AND
+-- NOT EQUAL TO
+-- the minimum salary
+-- of their own department.
+-- Show:
+-- - Employee name
+-- - Department ID
+-- - Salary
+-- Use:
+-- - One correlated subquery for AVG
+-- - One correlated subquery for MIN
+-- Sort by:
+-- 1. Department ID
+-- 2. Employee name (A-Z)
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.department_id,
+e1.salary
+FROM employees e1
+WHERE e1.salary <>
+(SELECT AVG(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+AND e1.salary <>
+(SELECT MIN(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+ORDER BY e1.department_id, e1.name ASC;
