0cb055cf2194924e0897c8c474ef003fcc3f72b5
Ronald <ronald.rgpineda@gmail.com>
2026-07-27 07:14:58 -0600

    Practice correlated subquery excluding department minimum and maximum salary

---
+++ Correlated_Subqueries/24_Correlated_Subqueries.sql
@@ -0,0 +1,34 @@
+-- ===========================================
+-- Correlated Subquery Exercise 24
+-- Business Request:
+-- Show employees whose salary is
+-- NOT EQUAL TO the department minimum
+-- AND
+-- NOT EQUAL TO the department maximum.
+-- Show:
+-- - Employee name
+-- - Department ID
+-- - Salary
+-- Use:
+-- - One correlated subquery for MIN
+-- - One correlated subquery for MAX
+-- Sort by:
+-- 1. Department ID
+-- 2. Salary (highest to lowest)
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.department_id,
+e1.salary
+FROM employees e1
+WHERE e1.salary <>
+(SELECT MIN(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+AND e1.salary <>
+(SELECT MAX(e2.salary)
+FROM employees e2
+WHERE e2.department_id = e1.department_id
+)
+ORDER BY e1.department_id, e1.salary DESC;
