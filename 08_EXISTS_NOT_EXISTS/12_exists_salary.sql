9e0e5d96d6f941e709d0c1f44e78ab6a3d187fe4
Ronald <ronald.rgpineda@gmail.com>
2026-07-30 07:23:52 -0600

    Practice SQL NOT EXISTS with BETWEEN salary filter

---
+++ 08_EXISTS_NOT_EXISTS/12_exists_salary.sql
@@ -0,0 +1,27 @@
+-- ===========================================
+-- NOT EXISTS Exercise 12
+-- Business Request:
+-- Show employees whose department
+-- does NOT exist
+-- AND
+-- whose salary is BETWEEN
+-- 1500 AND 2200.
+-- Show:
+-- - Employee name
+-- - Salary
+-- Use:
+-- NOT EXISTS
+-- Order by salary DESC.
+-- ===========================================
+-- My Solution
+SELECT e1.name,
+e1.salary
+FROM employees e1
+WHERE NOT EXISTS
+(
+SELECT 1
+FROM departments e2
+WHERE e2.department_id = e1.department_id
+)
+AND e1.salary BETWEEN 1500 AND 2200
+ORDER BY e1.salary DESC;
