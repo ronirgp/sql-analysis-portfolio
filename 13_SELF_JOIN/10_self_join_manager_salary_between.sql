7047d190df3477c22e8ec896322a1c29cc7e4701
Ronald <ronald.rgpineda@gmail.com>
2026-08-05 10:50:21 -0600

    Practice SQL SELF JOIN with salary range filters

---
+++ 13_SELF_JOIN/10_self_join_manager_salary_between.sql
@@ -0,0 +1,41 @@
+-- ===========================================
+-- SELF JOIN Exercise 10
+--
+-- Business Request:
+--
+-- Show:
+-- - Employee name
+-- - Manager name
+-- - Employee salary
+-- - Manager salary
+-- Show only employees who:
+-- 1. Have a manager.
+-- 2. Employee salary is
+--    BETWEEN 1400 AND 2500.
+-- 3. Manager salary is
+--    BETWEEN 2200 AND 3000.
+-- 4. Employee name ends with:
+--    a
+--    OR
+--    s
+-- Order by:
+-- 1. Manager salary DESC.
+-- 2. Employee salary DESC.
+-- 3. Employee name ASC.
+-- ===========================================
+-- My Solution
+SELECT e1.name AS employee_name,
+e2.name AS manager_name,
+e1.salary AS employee_salary,
+e2.salary AS manager_salary
+FROM employees e1
+INNER JOIN employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary BETWEEN 1400 AND 2500
+AND e2.salary BETWEEN 2200 AND 3000
+AND 
+(e1.name LIKE '%a'
+OR 
+e1.name LIKE '%s'
+)
+ORDER BY e2.salary DESC, e1.salary DESC, e1.name ASC;
