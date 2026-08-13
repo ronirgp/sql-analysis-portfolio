42ad17b493fe49635de63e5c8d6c2243fc0d7b6b
Ronald <ronald.rgpineda@gmail.com>
2026-08-13 07:26:42 -0600

    Analyze top two paid employees by department

--- 17_SQL_Analysis/05_top_two_paid_by_department.sql
+++ 17_SQL_Analysis/05_top_two_paid_by_department.sql
@@ -1,28 +1,32 @@
 -- ===========================================
 -- SQL Analysis Exercise 5
---
 -- Business Request:
---
 -- Management wants to see the TWO highest-paid
 -- employees from every department.
---
 -- Show:
---
 -- - Employee name
 -- - Department ID
 -- - Salary
---
 -- Return only the top 2 salary positions
 -- from each department.
---
 -- If two employees have the same salary,
 -- they should have the same ranking.
---
 -- Order by:
---
 -- 1. Department ID ASC
 -- 2. Salary DESC
---
 -- ===========================================
---
--- My Solution
+-- My Solution 
+WITH top_2_highest_paid AS
+(
+SELECT name,
+department_id,
+salary,
+RANK() OVER(PARTITION BY department_id ORDER by salary DESC
+) AS top_highest_paid
+FROM employees)
+SELECT name,
+department_id,
+salary
+FROM top_2_highest_paid
+WHERE top_highest_paid <= 2
+ORDER BY department_id ASC, salary DESC;
