6c6d6fc14828dd49ce2e547b80bd174d0e0fabf8
Ronald <ronald.rgpineda@gmail.com>
2026-08-25 07:42:30 -0600

    Filter departments by employee count and average salary

---
+++ 17_SQL_Analysis/35_department_above_average.sql
@@ -0,0 +1,20 @@
+-- Business Request:
+
+-- Management wants departments whose average salary is greater than $1,500 and that have at least 2 employees.
+
+-- Return:
+
+-- department ID
+-- employee count
+-- average salary
+
+-- Order by average salary descending
+-- My solution
+SELECT department_id,
+COUNT(*) AS total_employees, 
+AVG(salary) AS average_salary
+FROM employees
+GROUP BY department_id
+HAVING  average_salary > 1500
+AND COUNT(*) >= 2
+ORDER BY average_salary DESC;
