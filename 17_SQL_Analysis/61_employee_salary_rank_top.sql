fcc4524a10681c96842a43023e254e1a2cf1ca44
Ronald <ronald.rgpineda@gmail.com>
2026-09-01 07:35:06 -0600

    Identify highest-paid employees in each department

---
+++ 17_SQL_Analysis/61_employee_salary_rank_top.sql
@@ -0,0 +1,26 @@
+-- Business Request
+
+-- Management wants to identify the highest-paid employee in each department. 
+-- If multiple employees have the same highest salary, return all of them.
+
+-- Return:
+-- Employee name
+-- Department ID
+-- Salary
+WITH employee_salary AS
+(
+    SELECT name AS employee_name,
+           department_id,
+           salary AS employee_salary,
+           MAX(salary) OVER(
+               PARTITION BY department_id
+           ) AS highest_salary
+    FROM employees
+)
+SELECT employee_name,
+       department_id,
+       employee_salary
+FROM employee_salary
+WHERE employee_salary = highest_salary
+ORDER BY department_id ASC,
+         employee_salary DESC;
