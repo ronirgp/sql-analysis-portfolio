87ee700f2b75735726f6e71df1a2cce6e66b4c23
Ronald <ronald.rgpineda@gmail.com>
2026-09-04 08:06:22 -0600

    Identify employees at least below department highest salary

---
+++ 17_SQL_Analysis/75_employee_salary_vs_department_highest.sql
@@ -0,0 +1,30 @@
+-- Find employees whose salary is at least $700 below the highest salary in their department.
+
+-- Return:
+-- employee name
+-- department ID
+-- employee salary
+-- highest department salary
+-- amount below highest salary
+
+-- Only return employees meeting the $700 requirement.
+-- Order by amount_below_highest_salary descending.
+-- My solution
+WITH salary AS
+( 
+SELECT name AS employee_name,
+department_id,
+salary AS employee_salary,
+MAX(salary) OVER(PARTITION BY department_id
+) AS highest_department_salary
+FROM employees
+)
+SELECT employee_name,
+department_id,
+employee_salary,
+highest_department_salary,
+employee_salary - highest_department_salary AS
+amount_below_highest_salary
+FROM salary 
+WHERE employee_salary - highest_department_salary <= -700
+ORDER BY amount_below_highest_salary DESC;
