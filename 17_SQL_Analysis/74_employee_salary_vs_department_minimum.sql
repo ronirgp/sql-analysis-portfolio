d713c55257fb9202ecb86ce63927693a0aea53f7
Ronald <ronald.rgpineda@gmail.com>
2026-09-04 07:55:53 -0600

    Identify employees exactly above department lowest salary

---
+++ 17_SQL_Analysis/74_employee_salary_vs_department_minimum.sql
@@ -0,0 +1,30 @@
+-- Find employees whose salary is exactly $800 above the lowest salary in their department.
+
+-- Return:
+-- employee name
+-- department ID
+-- employee salary
+-- lowest department salary
+-- amount above lowest salary
+
+-- Only return employees meeting the requirement.
+-- Order by amount_above_lowest_salary descending.
+-- My solution
+WITH salary AS 
+(
+SELECT name AS employee_name,
+department_id,
+salary AS employee_salary,
+MIN(salary) OVER(PARTITION BY department_id
+) AS lowest_department_salary
+FROM employees
+)
+SELECT employee_name,
+department_id,
+employee_salary,
+lowest_department_salary,
+employee_salary - lowest_department_salary 
+amount_above_lowest_salary
+FROM salary 
+WHERE employee_salary - lowest_department_salary = 800
+ORDER BY amount_above_lowest_salary DESC;
