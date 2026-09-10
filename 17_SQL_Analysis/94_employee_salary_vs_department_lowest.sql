4235ad5353b25ba160c6bab45bb03c88530198ad
Ronald <ronald.rgpineda@gmail.com>
2026-09-10 07:32:38 -0600

    Find employees above department minimum salary

---
+++ 17_SQL_Analysis/94_employee_salary_vs_department_lowest.sql
@@ -0,0 +1,22 @@
+-- Show employees who earn more than $700 above the lowest salary in their department.
+--  Return the employee name, department ID, employee salary, lowest department salary,
+--  and the amount above the department minimum. Sort by the amount above the minimum from largest to smallest.
+-- My solution
+WITH salary AS 
+(
+SELECT name AS employee_name,
+department_id,
+salary AS employee_salary,
+MIN(salary) OVER(PARTITION BY department_id
+) AS lowest_department_salary
+FROM employees
+)
+SELECT employee_name,
+department_id,
+employee_salary,
+lowest_department_salary,
+employee_salary - lowest_department_salary AS
+amount_above_lowest_salary
+FROM salary
+WHERE employee_salary - lowest_department_salary > 700
+ORDER BY amount_above_lowest_salary DESC;
