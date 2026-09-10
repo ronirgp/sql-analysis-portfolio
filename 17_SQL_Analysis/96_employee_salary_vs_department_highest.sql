709518adb521c24bd4d4025af2ac416612765a25
Ronald <ronald.rgpineda@gmail.com>
2026-09-10 07:48:40 -0600

    Find employees below department maximum salary

---
+++ 17_SQL_Analysis/96_employee_salary_vs_department_highest.sql
@@ -0,0 +1,22 @@
+-- Show employees whose salary is at least $500 lower than the highest salary in their department.
+--  Return the employee name, department ID, employee salary, highest department salary,
+-- and the amount below the department maximum. Sort by the amount below the maximum from largest to smallest.
+-- MY solution
+WITH salary AS 
+(
+SELECT name AS employee_name,
+department_id,
+salary AS employee_salary,
+MAX(salary) OVER(PARTITION BY department_id
+) AS highest_department_salary
+FROM employees
+)
+SELECT employee_name,
+department_id,
+employee_salary,
+highest_department_salary,
+employee_salary -  highest_department_salary AS
+amount_below_department_salary
+FROM salary
+WHERE employee_salary -   highest_department_salary <= -500
+ORDER BY amount_below_department_salary DESC;
