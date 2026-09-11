3968f9f585815dd8a5e74f9fdc8429e4009096c6
Ronald <ronald.rgpineda@gmail.com>
2026-09-11 10:21:40 -0600

    Find employees earning less than their managers

---
+++ 17_SQL_Analysis/105_employee_manager_salary_analysis.sql
@@ -0,0 +1,14 @@
+-- “Show employees who earn less than their manager. Return the employee name, manager name, employee salary,
+--  manager salary, and the salary difference. Only include employees who have a manager.
+--  Sort by salary difference from smallest to largest.”
+SELECT e1.name AS employee_name,
+e2.name AS manager_name,
+e1.salary AS employee_salary,
+e2.salary AS manager_salary,
+e1.salary - e2.salary AS 
+salary_difference
+FROM employees e1
+INNER JOIN employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary < e2.salary 
+ORDER BY salary_difference ASC;
