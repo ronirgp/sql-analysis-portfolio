b9e3720ce9aef0cc43e10e72fafc3f0efbe95760
Ronald <ronald.rgpineda@gmail.com>
2026-09-12 07:17:03 -0600

    Compare employee and manager salaries

---
+++ 17_SQL_Analysis/109_employee_manager_salary_comparison.sql
@@ -0,0 +1,16 @@
+-- “Show employees who earn at least $300 more than their manager. Return the employee name, manager name,
+--  employee salary, manager salary, and the salary difference. Only include employees who have a manager.
+--  Sort by salary difference from largest to smallest.”
+-- My solution
+SELECT e1.name AS employee_name,
+e2.name AS manager_name,
+e1.salary AS employee_salary,
+e2.salary AS manager_salary,
+e1.salary - e2.salary AS 
+salary_difference
+FROM employees e1
+INNER JOIN employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary - e2.salary >= 300
+ORDER BY salary_difference DESC;
+
