10ddb9ee883722e0956b38da4106497f2e08b85b
Ronald <ronald.rgpineda@gmail.com>
2026-09-15 11:12:10 -0600

    Find employees earning more than managers

---
+++ 17_SQL_Analysis/146_employee_manager_salary_report.sql
@@ -0,0 +1,22 @@
+-- Business request:
+
+-- Find employees who earn more than their manager, but whose salary is less than $2000.
+
+-- Show:
+-- employee name
+-- manager name
+-- employee salary
+-- manager salary
+-- salary difference
+-- Sort by salary difference, largest to smallest.
+SELECT e1.name AS employee_name,
+       e2.name AS manager_name,
+       e1.salary AS employee_salary,
+       e2.salary AS manager_salary,
+       e1.salary - e2.salary AS salary_difference
+FROM employees e1
+INNER JOIN employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary > e2.salary
+  AND e1.salary < 2000
+ORDER BY salary_difference DESC;
